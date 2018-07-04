064ff9921fc4d37929827156dea78beb0de1c470
kumamon <takeda.singen@protonmail.com>
2018-07-04 23:44:16 +0300

    upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ps-4chan-dl
+# powershell-4chan-downloader
+powershell script to download images from 4chan.org threads
+## requirements
+1. windows 10
+2. you may need to change execution policy before using this script. to do so run the powershell console as admin user and run command ```Set-ExecutionPolicy Unrestricted```. change it back to ```Restricted``` after using the script. 
+## usage
+1. find a cool thread in 4chan.org 
+2. copy its url into clipboard
+3. run the script. it will check the clipboard for the link to your thread and dowload images automatically. if the script does not find a valid url to 4chan thread, it will prompt you for such url
+4. script saves images in separate directories for each thread. it will create directories in the current directory. the name of new directory will be truncated name of the thread (i.e. content of ```<title>``` tag)
+5. script downloads .jpg, .png and .gif images and also webm files, but please keep in mind that webms are big and a donwloading a long thread may take a while
---
+++ forchan_downloader.ps1
@@ -0,0 +1,42 @@
+﻿"4chan downloader`n"
+
+$threadUri = Get-Clipboard -Format Text;
+if (!($threadUri -match '^http:\/\/boards.4chan.org\/.*')) {
+$threadUri = Read-Host -Prompt "Enter thread URL with http";
+}
+$Response = Invoke-WebRequest -Uri $threadUri;
+
+$title = $($Response.ParsedHtml.getElementsByTagName('title')).innertext;
+$dirName = $title -replace '^\/.*\/ - (.*) - .* - .*$','$1';
+$dirName = $dirName -replace '[^a-zA-Z0-9]','-';
+$html = $Response.Links.Href;
+
+$regex = "^.*(\.jpg$)|(\.png$)|(\.gif$)|(\.webm$)";
+
+if (!(Test-Path $pwd\$dirName))
+{
+  "Creating directory $pwd\$dirName ...`n"
+  New-Item -ItemType  Directory -Path $pwd\$dirName | Out-Null;
+}
+else
+{
+  "Directory $dirName exists. Enter new name or press ctrl+c for exit.";
+  $dirName = Read-Host -Prompt "Enter new directory name.";
+  $dirName = $dirName -replace '[^a-zA-Z0-9]','-'; # just in case of slashes or other non-valid shit
+  New-Item -ItemType  Directory -Path $pwd\$dirName | Out-Null;
+}
+
+"Downloading pictures of thread '$title' into directory $dirName ...";
+
+$conter = 0;
+foreach ($currentHref in $html) {
+    if ($currentHref -match $regex) {
+        $Uri = 'http:' + $currentHref;
+        $fileName = $currentHref -replace '^.*/(\d+\.[a-zA-Z]{3})','$1';
+	if([System.IO.File]::Exists("$pwd\$dirName\$fileName")) { continue; }
+	"Downloading $fileName"
+        Invoke-WebRequest -Uri $Uri -OutFile "$pwd\$dirName\$fileName";
+	$counter++;
+    }
+}
+"$counter images downloaded`n";
