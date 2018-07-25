23857016dec70a89a00e541cbef30cce7be851d0
kumamon <takeda.singen@protonmail.com>
2018-07-25 21:09:40 +0300

    added -dest parameter

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ powershell script to download images from 4chan.org threads
 3. run the script. it will check the clipboard for the link to your thread and dowload images automatically. if the script does not find a valid url to 4chan thread, it will prompt you for such url
 4. script saves images in separate directories for each thread. it will create directories in the current directory. the name of new directory will be truncated name of the thread (i.e. content of ```<title>``` tag). if the directory already exists, script will prompt you for another name.
 5. script downloads .jpg, .png and .gif images and also webm files, but please keep in mind that webms are big and donwloading of a long thread may take a while
+## parameters
+```-dest``` is optional. you can specify another directory for files to download in this parameter. path must be absolute. please do not use special symbols in -dest path, specificaly [ and ], powershell's Invoke-WebRequest does not porcess them correctly. there will be a workaround later
--- forchan_downloader.ps1
+++ forchan_downloader.ps1
@@ -1,19 +1,46 @@
-﻿"4chan downloader`n"
+﻿Param 
+(
+    [string]$dest
+)
+"4chan downloader"
 
+if (!($dest)) 
+{
+    $dest = $pwd
+}
+else
+{
+    if (!([System.IO.Directory]::Exists("$dest")))
+    {
+        Write-Host "Path $dest not found, will write to $pwd"
+        $dest = $pwd;
+    }
+}
+
+$dest
 $threadUri = Get-Clipboard -Format Text;
 
 if (!($threadUri -match '^http:\/\/boards.4chan.org\/.*')) 
 {
-$threadUri = Read-Host -Prompt "Enter thread URL with http";
-if (!($threadUri -match '^http:\/\/boards.4chan.org\/.*'))
+    $threadUri = Read-Host -Prompt "Enter thread URL with http";
+    if (!($threadUri -match '^http:\/\/boards.4chan.org\/.*'))
+        {
+            "This is not 4chan url: $threadUri. `nExiting."
+            Exit(1);
+        }
+}
+
+try
 {
-"This is not 4chan url. Exiting."
-Exit(1);
+    $Response = Invoke-WebRequest -Uri $threadUri;
 }
+catch
+{
+    Write-Host "Connection error: `n";
+    Write-Host $error[0].Exception;
+    Write-Host "`nCheck the error message above";
+    exit; 
 }
-
-$Response = Invoke-WebRequest -Uri $threadUri;
-
 $title = $($Response.ParsedHtml.getElementsByTagName('title')).innertext;
 $dirName = $title -replace '^\/.*\/ - (.*) - .* - .*$','$1';
 $dirName = $dirName -replace '[^a-zA-Z0-9]','-';
@@ -21,30 +48,31 @@ $html = $Response.Links.Href;
 
 $regex = "^.*(\.jpg$)|(\.png$)|(\.gif$)|(\.webm$)";
 
-if (!(Test-Path $pwd\$dirName))
+if (!(Test-Path $dest\$dirName))
 {
-  "Creating directory $pwd\$dirName ...`n"
-  New-Item -ItemType  Directory -Path $pwd\$dirName | Out-Null;
+    "Creating directory $dest$dirName ...`n"
+    New-Item -ItemType  Directory -Path $dest\$dirName | Out-Null;
 }
 else
 {
-  "Directory $dirName exists. Enter new name or press ctrl+c for exit.";
-  $dirName = Read-Host -Prompt "Enter new directory name.";
-  $dirName = $dirName -replace '[^a-zA-Z0-9]','-'; # just in case of slashes or other non-valid shit
-  New-Item -ItemType  Directory -Path $pwd\$dirName | Out-Null;
+    "Directory $dirName exists. Enter new name or press ctrl+c for exit.";
+    $dirName = Read-Host -Prompt "Enter new directory name.";
+    $dirName = $dirName -replace '[^a-zA-Z0-9]','-'; # just in case of slashes or other non-valid shit
+    New-Item -ItemType  Directory -Path $dest\$dirName | Out-Null;
 }
 
-"Downloading pictures of thread '$title' into directory $dirName ...";
+"Downloading pictures of thread '$title' into directory $dest$dirName ...";
 
 $conter = 0;
 foreach ($currentHref in $html) {
-    if ($currentHref -match $regex) {
+    if ($currentHref -match $regex) 
+    {
         $Uri = 'http:' + $currentHref;
         $fileName = $currentHref -replace '^.*/(\d+\.[a-zA-Z]{3})','$1';
-	if([System.IO.File]::Exists("$pwd\$dirName\$fileName")) { continue; }
-	"Downloading $fileName"
-        Invoke-WebRequest -Uri $Uri -OutFile "$pwd\$dirName\$fileName";
-	$counter++;
+	    if([System.IO.File]::Exists("$dest\$dirName\$fileName")) { continue; }
+	    "Downloading $fileName"
+        Invoke-WebRequest -Uri $Uri -OutFile "$dest\$dirName\$fileName";
+	    $counter++;
     }
 }
 "$counter images downloaded`n";
